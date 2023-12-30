793a6bbf953c475e748a85d01f30fbca8a77c452
kanakWeb <kanakwebdev@gmail.com>
2023-12-30 11:50:27 +0600

    added new project

--- src/App.js
+++ src/App.js
@@ -1,23 +1,20 @@
-import logo from './logo.svg';
 import './App.css';
+import Navbar from './Components/Navbar.jsx'
+import Footer from './Components/Footer';
+import Routes from './Components/Routes.jsx';
+import { useState } from 'react';
+
+
 
 function App() {
+  const [darkTheme, SetDarkTheme] = useState(false)
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className={darkTheme ? 'dark' : ''}>
+      <div className='bg-gray-100 dark:bg-gray-900 dark:text-gray-200 min-h-screen'>
+       <Navbar/>
+       <Footer/>
+       <Routes/>
+      </div>
     </div>
   );
 }
